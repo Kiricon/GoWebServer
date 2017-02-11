567ef4c59d1d143c341d9ac650c0a1543ae95ac4
kiricon <kiricon@live.com>
2017-02-11 13:48:44 -0700

    Multple routes are now working.

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Println(r.Method, r.RequestURI)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Main Handler")
 	fmt.Println(r.Method, r.RequestURI)
 }
 
@@ -14,7 +19,8 @@ func main() {
 
 	port := ":3000"
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/", mainHandler)
 	http.ListenAndServe(port, nil)
 	fmt.Println("Go Server is now running on port", port)
 }
